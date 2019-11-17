4d520ff114a9f7430d6bf03a3a72f54e7179f3ab
Dipack P Panjabi <dipack.panjabi@gmail.com>
2019-11-16 21:56:21 -0500

    Update some results ;

--- kaggle/kaggle.py
+++ kaggle/kaggle.py
@@ -2,6 +2,7 @@ import os
 import datetime
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 BASE_LOC = "./data"
@@ -30,20 +31,34 @@ def train_predict(learner, x, y, x_test):
     return learner.predict(x_test)
 
 def get_outfile_name():
-    return "{0}/submission_{1}.csv".format(OUTPUT_LOC, datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S"))
+    return "{0}/submission_{1}.csv".format(OUTPUT_LOC, datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
 
 def main():
     if not os.path.isdir(OUTPUT_LOC):
         os.makedirs(OUTPUT_LOC, exist_ok=True)
+
+    # Gave 0.83006
+    # learner = RandomForestClassifier(n_estimators=50, criterion="gini", max_depth=5, min_samples_split=2, bootstrap=True)
+
+    # Gave 0.80757 - bad settings?
+    # learner = SVC(C=1.0, kernel="rbf")
+
+    # Gave 0.83934
+    # learner = RandomForestClassifier(n_estimators=100, criterion="gini", max_depth=6, min_samples_split=5, bootstrap=True)
+
+    # Gave 0.83934
+    learner = RandomForestClassifier(n_estimators=100, criterion="gini", max_depth=6, min_samples_split=8, bootstrap=True)
+
     x, y = get_train_data()
     x_test = get_test_data()
-    rf = RandomForestClassifier(n_estimators=50, criterion="gini", max_depth=5, min_samples_split=2, bootstrap=True)
     x_test_vals = x_test.loc[:, 1:]
-    predictions = train_predict(rf, x, y, x_test_vals)
+    predictions = train_predict(learner, x, y, x_test_vals)
     out = create_output(x_test.index, predictions)
-    out.to_csv(get_outfile_name(), index=None, header=True)
     print(predictions)
     print(out)
+    outfn = get_outfile_name()
+    print("Writing to {}".format(outfn))
+    out.to_csv(outfn, index=None, header=True)
     return
 
 if __name__ == "__main__":
