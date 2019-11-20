6e284ea1d5ab2e66b228edfbb6ec071ddd9a40fb
Dipack P Panjabi <dipack.panjabi@gmail.com>
2019-11-20 18:27:38 -0500

    Update PCA scores

--- kaggle/kaggle.py
+++ kaggle/kaggle.py
@@ -5,6 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.preprocessing import RobustScaler, MinMaxScaler, StandardScaler
+from sklearn.decomposition import PCA
 
 BASE_LOC = "./data"
 TRAIN = "{0}/train_features.csv".format(BASE_LOC)
@@ -51,15 +52,33 @@ def main():
     # With RobustScaler - 0.83828
     # With MinMaxScaler - 0.81528
     # With StandardScaler - 0.83278
-    learner = RandomForestClassifier(n_estimators=100, criterion="gini", max_depth=6, min_samples_split=8, bootstrap=True)
+    learner = RandomForestClassifier(n_estimators=100, criterion="gini", max_depth=8, min_samples_split=10, bootstrap=True)
 
+
+    # With 20 features and RF - 0.82154
+    # With 50 features and RF - 0.84745
+    pca = PCA(n_components=50)
+
+    # Get data
     x, y = get_train_data()
+
+    # Fit x and get most relevant features
+    pca.fit(x)
+    x = pca.transform(x)
+
+    # Get test data
     x_test = get_test_data()
     x_test_vals = x_test.loc[:, 1:]
+
+    # Get most relevant features
+    x_test_vals = pca.transform(x_test_vals)
+
     predictions = train_predict(learner, x, y, x_test_vals)
     out = create_output(x_test.index, predictions)
+
     print(predictions)
     print(out)
+
     outfn = get_outfile_name()
     print("Writing to {}".format(outfn))
     out.to_csv(outfn, index=None, header=True)
